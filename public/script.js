f71e12fd4093c7d623804e339b71105cf16a1b6b
Artworqq <keysub.kevin@gmail.com>
2025-11-16 03:14:17 -0500

    Delete public/script.js

--- public/script.js
+++
@@ -1,9 +0,0 @@
-// public/src/script.ts
-document.addEventListener("DOMContentLoaded", () => {
-  const startBtn = document.getElementById("start-btn");
-  if (startBtn) {
-    startBtn.addEventListener("click", () => {
-      window.location.href = "./info.html";
-    });
-  }
-});
